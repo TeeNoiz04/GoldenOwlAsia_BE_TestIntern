713d189aa9bcef919937554edd4870307ab2f15d
DuocDVT <thanhduoc020919@gmail.com>
2026-01-18 12:57:42 +0700

    feat: add request rate limiting

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\StatisticController;
+
 Route::get('/ping', function () {
     return response()->json(['message' => 'api ok']);
 });
 
-// Student Routes
-Route::get('/students/top', [StudentController::class, 'topStudents']);
-Route::get('/students/{registrationNumber}', [StudentController::class, 'show'])->whereNumber('registrationNumber');
+// Student Routes - Rate limit: 60 requests per minute
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/students/top', [StudentController::class, 'topStudents']);
+    Route::get('/students/{registrationNumber}', [StudentController::class, 'show'])->whereNumber('registrationNumber');
+});
 
-// Statistic Routes
-Route::get('/statistics/student-summary', [StatisticController::class, 'studentSummary']);
-Route::get('/statistics/subject-statistics-bar', [StatisticController::class, 'subjectStatisticsBar']);
-Route::get('/statistics/subject-statistics-line', [StatisticController::class, 'subjectStatisticsLine']);
+// Statistic Routes - Rate limit: 60 requests per minute
+Route::middleware('throttle:60,1')->group(function () {
+    Route::get('/statistics/student-summary', [StatisticController::class, 'studentSummary']);
+    Route::get('/statistics/subject-statistics-bar', [StatisticController::class, 'subjectStatisticsBar']);
+    Route::get('/statistics/subject-statistics-line', [StatisticController::class, 'subjectStatisticsLine']);
+});
